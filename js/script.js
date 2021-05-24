f64c66807514e93c0eff0ec98914cd3d76eb5216
Ignacio Aguirre <ignacio24687531@gmail.com>
2021-05-24 15:25:46 -0500

    📴📴finished calling api
    Finished calling api and added a different font

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
+document.getElementById("search-value").addEventListener("keyup", function (e) {
+  if (e.keyCode === 13) {
+    document.getElementById("button-search").click();
+  }
+});
+
+/*api call*/
 const access_key = "554ece73b31f79e5b5912ca5adce3cf5";
-const baseUrl = "http://apilayer.net/api/check?access_key=";
+const baseUrl = "https://apilayer.net/api/check?access_key=";
 const searchButton = document.getElementById("button-search");
 let searchValue = document.getElementById("search-value");
 let searchEmailData = document.getElementById("search-dataEmail");
 let searchDomainData = document.getElementById("search-dataDomain");
+let searchFormData = document.getElementById("search-dataForm");
+let searchDisposableData = document.getElementById("search-dataDisposable");
+let searchFreeData = document.getElementById("search-dataFree");
+let searchMeanData = document.getElementById("search-dataMean");
 searchButton.addEventListener("click", () => {
   fetch(
     baseUrl + access_key + "&email=" + searchValue.value + "&smtp=1&format=1"
@@ -12,11 +23,31 @@ searchButton.addEventListener("click", () => {
     .then((data) => {
       /*email data*/
       let emailData = data["email"];
-      searchEmailData.textContent = "Email Address " + emailData;
+      searchEmailData.textContent = "Email Address:  " + emailData;
       /*end email data*/
       /*domain data*/
       let domainData = data["domain"];
-      searchDomainData.textContent = "domain" + domainData;
+      searchDomainData.textContent = "domain:    " + domainData;
       /*end domain data*/
+      /*valid form?*/
+      let formData = data["format_valid"];
+      searchFormData.textContent = "Valid Format:    " + formData;
+      /*end valid form?*/
+      /*searchDisposableData*/
+      let disposableData = data["disposable"];
+      searchDisposableData.textContent = "Disposable:    " + disposableData;
+      /*end searchDisposableData*/
+      /*free*/
+      let freeData = data["free"];
+      searchFreeData.textContent = "Free: " + freeData;
+      /*end free*/
+
+      /*did you mean*/
+      let didYouMeanData = data["did_you_mean"];
+      searchMeanData.textContent = "Did You Mean: " + didYouMeanData;
+      /*end did you mean*/
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });
